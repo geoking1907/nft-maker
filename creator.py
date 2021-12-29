bb259fbdcb056415c14e205252d3e96c0998e138
geoking1907 <geo190708@outlook.com>
2021-12-29 18:18:51 +0300

    images.py file

--- creator.py
+++ creator.py
@@ -2,38 +2,6 @@ from PIL import Image
 import random
 from images import *
 
-backgrounds = [
-    'images/b/b1.png',
-    'images/b/b2.png',
-    'images/b/b3.png'
-]
-faces = [
-    'images/f/f1.png',
-    'images/f/f2.png',
-    'images/f/f3.png',
-    'images/f/f4.png',
-    'images/f/f5.png',
-    'images/f/f6.png'
-]
-head_accessoires = [
-    'images/h/h1.png',
-    'images/h/h2.png',
-    'images/h/h3.png'
-]
-glasses = [
-    'images/g/g1.png',
-    'images/g/g2.png',
-    'images/g/g3.png',
-    'images/g/g4.png'
-]
-t_shirts = [
-    'images/t/t1.png',
-    'images/t/t2.png',
-    'images/t/t3.png',
-    'images/t/t4.png',
-    'images/t/t5.png'
-]
-
 
 def create():
     background = random.choices(backgrounds, weights=(50, 13, 37))[0]
